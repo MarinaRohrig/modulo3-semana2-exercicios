321ea1f272a5bdc2de90bda227f3a4ea4cde924e
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-07 20:23:46 -0300

    criado doget na classe exercicio3

--- src/main/java/br/com/futurodev/exercicios/Exercicio3.java
+++ src/main/java/br/com/futurodev/exercicios/Exercicio3.java
@@ -1,4 +1,16 @@
 package br.com.futurodev.exercicios;
 
-public class Exercicio3 {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Exercicio3 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       req.getRequestDispatcher("modelo_formulario.html").forward(req, resp);
+    }
 }
