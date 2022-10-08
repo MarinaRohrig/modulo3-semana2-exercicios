560c263d542dd4023d317c4e3f632434abd535ed
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-07 22:09:04 -0300

    ajuste exercicio3resultado

--- src/main/java/br/com/futurodev/exercicios/Exercicio3Resultado.java
+++ src/main/java/br/com/futurodev/exercicios/Exercicio3Resultado.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class Exercicio3CadastroResultado extends HttpServlet {
+public class Exercicio3Resultado extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
@@ -23,6 +23,20 @@ public class Exercicio3CadastroResultado extends HttpServlet {
         String nota3 = req.getParameter("nota3");
         String nota4 = req.getParameter("nota4");
 
+        Double nota1dbl = Double.parseDouble(nota1);
+        Double nota2dbl = Double.parseDouble(nota2);
+        Double nota3dbl = Double.parseDouble(nota3);
+        Double nota4dbl = Double.parseDouble(nota4);
 
+
+        writer.println("<h3><b>"+nome+"</b></h3>");
+        writer.println("<ul>");
+        writer.println("<li>Disciplina: <b>"+disciplina+"</b></li>");
+        writer.printf("</br><li>Nota 1:   <b>%.2f</b></li>",nota1dbl,"");
+        writer.printf("<li>Nota 2:   <b>%.2f</b></li>",nota2dbl,"");
+        writer.printf("<li>Nota 3:   <b>%.2f</b></li>",nota3dbl,"");
+        writer.printf("<li>Nota 4:   <b>%.2f</b></li>",nota4dbl,"");
+        writer.printf("</br><li>Média:     <b>%.2f</b></li>",((nota1dbl+nota2dbl+nota3dbl+nota4dbl)/4),"");
+        writer.println("</ul>");
     }
 }
