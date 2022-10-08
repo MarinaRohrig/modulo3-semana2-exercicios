3b936dd237544e225ec8d9b542f9258ea03b2ca3
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-07 22:21:43 -0300

    + arquivo formulario post e .java do ex 4

---
+++ src/main/java/br/com/futurodev/exercicios/Exercicio4.java
@@ -0,0 +1,16 @@
+package br.com.futurodev.exercicios;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Exercicio4 extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
